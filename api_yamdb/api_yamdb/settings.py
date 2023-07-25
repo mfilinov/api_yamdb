ec030836b4a88db537a124bfbdbe71aa263ff806
Евгений Зинин <fryzgh@yandex.ru>
2023-07-25 17:16:44 +0300

    Доработаны модели. В settings добавлена переменная.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -101,3 +101,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+
+TEXT_MAX_LENGTH = 30
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,28 +2,35 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+from api_yamdb.settings import TEXT_MAX_LENGTH
+
 User = get_user_model()
 
 
 class Review(models.Model):
     """Модель отзыва на произведение."""
-    text = models.TextField()
+    text = models.TextField(verbose_name='Текст отзыва')
     #  Оценка произведения.
-    score = models.IntegerField(
+    score = models.PositiveIntegerField(
         verbose_name='Оценка',
-        validators=[MinValueValidator(1), MaxValueValidator(10)]
+        validators=[
+            MinValueValidator(1, message='Ваша оценка ниже допустимой'),
+            MaxValueValidator(10, message='Ваша оценка выше допустимой')
+        ]
     )
     #  Наименование автора. Переделать после создания модеди User.
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='reviews',
+        verbose_name='Автор'
     )
     #  Наименование произведения на которое пишется отзыв.
     title = models.ForeignKey(
         'Title',  # Убрать ковычки после создания модели Title.
         on_delete=models.CASCADE,
-        related_name='reviews'
+        related_name='reviews',
+        verbose_name='Произведение'
     )
     pub_date = models.DateTimeField(
         'Дата добавления',
@@ -31,26 +38,47 @@ class Review(models.Model):
         db_index=True
     )
 
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ('-pub_date',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=('author', 'title'),
+                name='unique_review'
+            )
+        ]
+
     def __str__(self):
-        return self.text
+        return self.text[:TEXT_MAX_LENGTH]
 
 
 class Comment(models.Model):
     """Модель комментария к отзыву."""
-    text = models.TextField()
+    text = models.TextField(verbose_name='Текст комментария')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='comments'
+        related_name='comments',
+        verbose_name='Автор'
     )
     #  Под каким отзывом пишется комментарий.
     review = models.ForeignKey(
         Review,
         on_delete=models.CASCADE,
-        related_name='comments'
+        related_name='comments',
+        verbose_name='Отзыв'
     )
     pub_date = models.DateTimeField(
         'Дата добавления',
         auto_now_add=True,
         db_index=True
     )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text[:TEXT_MAX_LENGTH]
