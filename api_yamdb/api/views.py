43e678a38170eaad9929379c7e21f6efb5e88193
Евгений Волобуев <afondurin@yandex.ru>
2023-07-25 16:42:56 +0300

    Add views/serializers for title/category/genre

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import TitleSerializer, CategorySerializer, GenreSerializer
+from reviews.models import Title, Category, Genre
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
