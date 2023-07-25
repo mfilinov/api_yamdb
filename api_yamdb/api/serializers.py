bee2fd5b3c79a6943099b556375d3c982eeb4ad5
Евгений Зинин <fryzgh@yandex.ru>
2023-07-25 18:51:31 +0300

    Добавлены api/urls

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -25,3 +25,44 @@ class TitleSerializer(serializers.ModelSerializer):
         model = Title
         fields = ('id', 'name', 'year', 'rating', 'description',
                   'genre', 'category')
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from reviews.models import Comment, Title, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Serializer для модели Review."""
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+    title = serializers.SlugRelatedField(
+        queryset=Title.objects.all(),
+        slug_field='name'
+    )
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ['title']
+        #  Каждый пользователь может оставить только один отзыв на произведение
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Review.objects.all(),
+                fields=('author', 'title')
+            )
+        ]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Serializer для модели Comment."""
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ['review']
---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from api.views import CommentViewSet, ReviewViewSet
+
+router = routers.DefaultRouter()
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+
+urlpatterns = [
+    path('v1/', include(router.urls))
+]
