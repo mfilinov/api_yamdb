50642cb131aa03299bc85723005ef2f09ed606cf
Евгений Зинин <fryzgh@yandex.ru>
2023-07-25 15:32:56 +0300

    Создание моделей Review, Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Review(models.Model):
+    """Модель отзыва на произведение."""
+    text = models.TextField()
+    #  Оценка произведения.
+    score = models.IntegerField(
+        verbose_name='Оценка',
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    #  Наименование автора. Переделать после создания модеди User.
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    #  Наименование произведения на которое пишется отзыв.
+    title = models.ForeignKey(
+        'Title',  # Убрать ковычки после создания модели Title.
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    """Модель комментария к отзыву."""
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    #  Под каким отзывом пишется комментарий.
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
