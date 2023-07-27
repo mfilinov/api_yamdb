c9be01a2fb73a2d9ba5330e2c66f352491c5da6f
Maksim Filinov <mfilinov@yandex.ru>
2023-07-27 21:28:37 +0300

    Исправил сериалайзер, чтобы отдавал только токен

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -31,7 +31,9 @@ class TokenView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = TokenSerializer(data=request.data)
         if serializer.is_valid():
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(
+                serializer.validated_data,
+                status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
