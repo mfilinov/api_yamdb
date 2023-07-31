1b10f98ff9d858b7214ca5cf9827793086d1d4b6
Maksim Filinov <mfilinov@yandex.ru>
2023-07-31 21:11:55 +0300

    Добавлена поддержка аргументов

--- api_yamdb/reviews/management/commands/import.py
+++ api_yamdb/reviews/management/commands/import.py
@@ -8,8 +8,22 @@ from users.models import User
 class Command(BaseCommand):
     help = 'Фиктивное наполнение БД'
 
-    def handle(self, *args, **options):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-d',
+            '--delete',
+            action="store_true",
+            help='Удаление записей из базы данных',
+        )
+
+    def _import_users(self):
         with open('static/data/users.csv') as f:
             reader = csv.DictReader(f)
             User.objects.bulk_create([User(**row) for row in reader])
         self.stdout.write('Пользователи успешно добавлены')
+
+    def handle(self, *args, **options):
+        if options['delete']:
+            User.objects.all().delete()
+        else:
+            self._import_users()
