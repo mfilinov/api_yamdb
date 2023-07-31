fdf221b3f9b3f940bab30cc39b4c67b036b91d1d
Maksim Filinov <mfilinov@yandex.ru>
2023-07-31 22:23:13 +0300

    Добавляются категории, после изменения модели Title заработает их добавление

--- api_yamdb/reviews/management/commands/import.py
+++ api_yamdb/reviews/management/commands/import.py
@@ -2,6 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 
+from reviews.models import Category, Title
 from users.models import User
 
 
@@ -22,8 +23,23 @@ class Command(BaseCommand):
             User.objects.bulk_create([User(**row) for row in reader])
         self.stdout.write('Пользователи успешно добавлены')
 
+    def _import_categories(self):
+        with open('static/data/category.csv') as f:
+            reader = csv.DictReader(f)
+            Category.objects.bulk_create([Category(**row) for row in reader])
+        self.stdout.write('Категории успешно добавлены')
+
+    def _import_titles(self):
+        with open('static/data/titles.csv') as f:
+            reader = csv.DictReader(f)
+            Title.objects.bulk_create([Title(**row) for row in reader])
+        self.stdout.write('Статьи успешно добавлены')
+
     def handle(self, *args, **options):
         if options['delete']:
             User.objects.all().delete()
+            Category.objects.all().delete()
         else:
             self._import_users()
+            self._import_categories()
+            # self._import_titles()
