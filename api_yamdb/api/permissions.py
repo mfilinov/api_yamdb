950f36b52f06fffe5a4a72b06380fc734f5f2aed
Maksim Filinov <mfilinov@yandex.ru>
2023-08-03 17:52:12 +0300

    delete unused files

--- api_yamdb/api/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import permissions
-
-
-class AdminPermission(permissions.IsAuthenticatedOrReadOnly):
-    def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return request.user.Role.ADMIN
